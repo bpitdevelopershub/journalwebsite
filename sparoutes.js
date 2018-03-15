db23ca09b0688ec79f0d7c777870dd2974b222ae
ARPIT CHOUDHARY <choudharyarpit99@gmail.com>
2018-03-15 22:31:24 +0530

    link

--- sparoutes.js
+++ sparoutes.js
@@ -4,8 +4,13 @@ app.config(function($routeProvider,$locationProvider){
         templateUrl:'views/home.html'
     }).when('/about',{
         templateUrl:'views/about.html',
+    }).when('/login',{
+        templateUrl:'views/login.html',
+    }).when('/archive',{
+        templateUrl:'views/archive.html',
+    }).when('/current',{
+        templateUrl:'views/current.html',
     }).when('/search',{
-        templateUrl:'views/search.html',
-        controller:'newsctrl' 
+        templateUrl:'views/search.html', 
     }).otherwise({template:'No Data Available'});
 });
