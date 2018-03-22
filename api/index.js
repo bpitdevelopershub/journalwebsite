242b5f5738e99c81bf19d51160bc79a1308d4872
Nikhil Thakur <nikhilthakur3197@gmail.com>
2018-03-22 17:02:48 +0530

    login data recieved in nodejs

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const bodyparser = require('body-parser');
 mongoose.connect('mongodb://Journal:12345@ds113849.mlab.com:13849/bpit-journal-services');
 app.set('view engine','ejs');
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
 
 var schema = new mongoose.Schema({
     vol : Number,
@@ -44,3 +45,30 @@ app.get('/archive',function(request,response){
 app.listen(3000,function(){
     console.log('listening to port 3000');
 })
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+app.post('/login',function(request,response){
+    var email = request.body.email;
+    var password = request.body.password;
+    // console.log(email, password);
+});
--- js/factory/loginfactory.js
+++ js/factory/loginfactory.js
@@ -3,7 +3,9 @@ app.factory('login',function($q,$http){
         doLogin(data){
             var defer = $q.defer();
             var url = "http://localhost:3000/login";
-            $http.post(url,data).then((data)=>{
+            $http.post(url,data, {headers: {
+                'Content-Type': 'application/json'
+            }}).then((data)=>{
                 defer.resolve(data.data);
             },(err)=>{
                 defer.reject(err);
